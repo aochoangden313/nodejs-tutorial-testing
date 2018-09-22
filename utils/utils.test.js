9e9da2e4b90de1736bcf7592d977730ece3bb6b8
unknown <quanlv.56@gmail.com>
2018-09-22 22:57:33 +0700

    testing assertion with 'expect' lib

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,31 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('should add 2 numbers', () => {
     let res = utils.add(33, 11);
 
-    if (res != 44) {
-        throw new Error(`Do not return 44 actual value is ${res}`);
-    }
+    expect(res).toBe(44).toBeA('number');
 });
 
 it('It should return square value', () => {
     let res = utils.square(9);
 
-    if (res != 81) {
-        throw new Error(`Do not return 81 actual value is ${res}`);
-    }
+    expect(res).toBe(81).toBeA('number');
+});
+
+// should verify first name and last name are set
+// assert it includes firstName and lastName with proper values
+it('set user with first name and last name', () => {
+    let user = {location: 'Ha Noi', age: 25};
+    let result = utils.setName(user, 'Quan Le');
+
+    // expect(result).toEqual({
+    //     firstName: 'Quan',
+    //     lastName: 'Le'
+    // });
+
+    expect(result).toInclude({
+        firstName: 'Quan',
+        lastName: 'Le'
+    });
 });
